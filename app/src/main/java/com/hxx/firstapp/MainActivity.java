f1ed634659bf43f2051f8d13ab48e28b0fe7f9b6
harper0201 <963444707@qq.com>
2021-09-03 23:56:59 -0400

    third commit(add some new function)

--- app/src/main/java/com/hxx/firstapp/MainActivity.java
+++ app/src/main/java/com/hxx/firstapp/MainActivity.java
@@ -3,16 +3,22 @@ package com.hxx.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText username,password;
     Button login_in;
+    TextView forget_password;
+    TextView hello_world;
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +28,11 @@ public class MainActivity extends AppCompatActivity {
         password = findViewById(R.id.password);
         login_in = findViewById(R.id.login_in);
         login_in.setOnClickListener(new MyOnClickListener());
-    }
+        forget_password = findViewById(R.id.forget_password);
+        forget_password.setOnClickListener(new SecondClickListener());
+        hello_world = findViewById(R.id.hello_world);
 
+    }
 
     class MyOnClickListener implements View.OnClickListener{
         @Override
@@ -33,9 +42,21 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
             Toast.makeText(MainActivity.this,"Welcome,"+username.getText(),Toast.LENGTH_SHORT).show();
+            hello_world.animate().rotationBy(180).start();
+            hello_world.animate().alpha(0).setDuration(1000).start();
+
         }
 
     }
+
+    class SecondClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            intent = new Intent (MainActivity.this,forgetpassword.class);
+            startActivity(intent);
+        }
+    }
+
 
 
 
